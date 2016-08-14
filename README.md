bbf30de0304419e33155340f5b4c1ed288b8f597
mayuelei <mayuelei@meituan.com>
2016-08-15 01:35:57 +0800

    2.add-two-numbers

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ the solutions for leetcode by javascript
 ##index
 
 1.[Two Sum]([source](https://github.com/mayuelei66/leetcode_javascript/blob/master/1.md))
+1.[Add Two Numbers]([source](https://github.com/mayuelei66/leetcode_javascript/blob/master/1.md))
---
+++ code/2.add-two-numbers.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/*
+* Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+* Output: 7 -> 0 -> 8
+* */
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    var sumNode = new ListNode(-1),
+    result = sumNode,
+    ext = 0,
+    sumVal = 0,
+    val = 0;
+    while (l1 || l2 || ext !== 0 ){
+        var v1Val = l1.val || 0;
+        var v2Val = l2.val || 0;
+        sumVal = v1Val + v2Val + ext;
+        val = sumVal >= 10 ? sumVal%10 : sumVal;
+        ext = sumVal >= 10 ? 1 : 0;
+        sumNode.next = new ListNode(val);
+        sumNode = sumNode.next;
+        l1 = l1 ? l1.next : {};
+        l2 = l2 ? l2.next : {};
+    }
+    return result.next;
+};
